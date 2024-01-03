9e7517dfc5f11b7ed38a8faae685f1e878187cae
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-03 06:09:03 -0800

    task4

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def matchCase(str)
+  regex = /hbt*n/
+  return str.scan(regex).join
+end
+
+if ARGV.length == 1
+  puts matchCase(ARGV[0])
+else
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+end
