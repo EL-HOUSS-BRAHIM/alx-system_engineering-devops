13c951ac96c752cb763afbfd5c54e386f367234f
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-02-29 00:31:42 +0100

    new

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/pup
+# installation (python && Flask && werkzeug).
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
