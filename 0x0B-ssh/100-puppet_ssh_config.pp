66db73c16be0ff6b449e6df969cd3244dde8cc2b
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-02-29 00:26:16 +0100

    now

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Client configuration file (w/ Puppet)
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
