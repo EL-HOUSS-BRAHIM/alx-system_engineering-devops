901d04e9ccda8a54b98439e9088739d181a77702
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-03-06 16:54:47 +0100

    load_balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# Add a custom HTTP header with Puppet
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
