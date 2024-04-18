9f8fc58be816b4420a0ace31c49ed6218902de19
next-gen bross <bross.or.of@gmail.com>
2024-04-18 13:46:44 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
-# File: 0-the_sky_is_the_limit_not.pp
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# limit
+exec { 'update -> ulimit':
+  command  => "sed -i 's/^ULIMITED=.*/ULIMITED=\"-n 15000\"/' /etc/default/nginx",
+  provider => 'shell',
 }
 
+-> exec { 'restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+}
